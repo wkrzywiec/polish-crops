9952afc49ccc1ae285b569c7e8cb27c2971785a4
Wojtek Krzywiec <wojciechkrzywiec@gmail.com>
2020-05-06 14:52:18 +0200

    add comments to eurostat tools

--- tools/eurostat.py
+++ tools/eurostat.py
@@ -4,6 +4,14 @@ import tools.data_helper as helper
 __raw_data_directory = 'data/raw/'
 
 def download_crop_prices(url = "https://ec.europa.eu/eurostat/estat-navtree-portlet-prod/BulkDownloadListing?sort=1&downfile=data/apri_ap_crpouta.tsv.gz"):
+    """Download & unzip 'Selling prices of crop products (absolute prices) - annual price (from 2000 onwards)' report from Eurostat
+
+    Keyword Arguments:
+        url {str} -- URL of a report (default: {"https://ec.europa.eu/eurostat/estat-navtree-portlet-prod/BulkDownloadListing?sort=1&downfile=data/apri_ap_crpouta.tsv.gz"})
+
+    Returns:
+        str -- path to saved file
+    """
     content = helper.download_file(url)
     file_name = url.split('/')[-1]
     zipped_file_path = helper.save_file(content, __raw_data_directory + file_name)
@@ -12,6 +20,14 @@ def download_crop_prices(url = "https://ec.europa.eu/eurostat/estat-navtree-port
     return unzipped_file
     
 def download_crop_categories_dic(url = "https://ec.europa.eu/eurostat/estat-navtree-portlet-prod/BulkDownloadListing?sort=1&downfile=dic/en/prod_veg.dic"):
+    """Download crop categories dictionary
+
+    Keyword Arguments:
+        url {str} -- URL of a dictionary (default: {"https://ec.europa.eu/eurostat/estat-navtree-portlet-prod/BulkDownloadListing?sort=1&downfile=dic/en/prod_veg.dic"})
+
+    Returns:
+        str -- path to saved file
+    """
     content = helper.download_file(url)
     file_name = url.split('/')[-1]
     raw_file_path = helper.save_file(content, __raw_data_directory + file_name)
