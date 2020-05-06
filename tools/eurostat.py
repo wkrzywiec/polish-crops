5e7451bfd8e2812e072c754c2e34db0060b4e249
Wojtek Krzywiec <wojciechkrzywiec@gmail.com>
2020-05-06 14:26:14 +0200

    add download crop categories file script

--- tools/eurostat.py
+++ tools/eurostat.py
@@ -2,32 +2,37 @@ import requests
 import gzip
 import os
 
+__raw_data_directory = 'data/raw/'
+
 def download_crop_prices(url = "https://ec.europa.eu/eurostat/estat-navtree-portlet-prod/BulkDownloadListing?sort=1&downfile=data/apri_ap_crpouta.tsv.gz"):
-    content = __download_raw_file(url)
+    content = __download_file(url)
     file_name = url.split('/')[-1]
-    raw_file_path = __save_raw_file(content, file_name)
-    unzipped_file = __unzip_gz_file(raw_file_path)
-    os.remove(raw_file_path)
+    zipped_file_path = __save_file(content, __raw_data_directory + file_name)
+    unzipped_file = __unzip_gz_file(zipped_file_path)
+    os.remove(zipped_file_path)
     return unzipped_file
     
-def download_crop_categories_dic(url = "https://ec.europa.eu/eurostat/estat-navtree-portlet-prod/BulkDownloadListing?sort=1&downfile=dic%2Fen%2Fprod_veg.dic"):
-    pass
+def download_crop_categories_dic(url = "https://ec.europa.eu/eurostat/estat-navtree-portlet-prod/BulkDownloadListing?sort=1&downfile=dic/en/prod_veg.dic"):
+    content = __download_file(url)
+    file_name = url.split('/')[-1]
+    raw_file_path = __save_file(content, __raw_data_directory + file_name)
+    return raw_file_path
 
-def __download_raw_file(url: str):
+def __download_file(url: str):
     response = requests.get(url)
     if response.status_code != 200:
         raise SystemExit('Unable to download data file. Reason: {} {}'.format(response.status_code, response.reason))
     return response.content
 
-def __save_raw_file(content, file_name: str) -> str:
-    raw_file_path = 'data/raw/{}'.format(file_name)
-    with open(raw_file_path, 'wb') as f: 
+def __save_file(content, file_path: str) -> str:
+    with open(file_path, 'wb') as f: 
         f.write(content)
-    return raw_file_path
+    return file_path
 
-def __unzip_gz_file(raw_file_path):
-    file_name = 'crop-prices.tsv'
-    with gzip.open(raw_file_path, 'rb') as f:
-        __save_raw_file(f.read(), file_name)
-    return 'data/raw/{}'.format(file_name)
+def __unzip_gz_file(file_path):
+    gz_extension_index = file_path.rfind('.')
+    unziped_file_path = file_path[:gz_extension_index]
+    with gzip.open(file_path, 'rb') as f:
+        __save_file(f.read(), unziped_file_path)
+    return unziped_file_path
     
--- tools/tests/test_eurostat.py
+++ tools/tests/test_eurostat.py
@@ -5,11 +5,15 @@ class TestEurostat(unittest.TestCase):
 
     def test_download_economic_accounts_for_agri(self):
         eurostat.download_crop_prices()
-        open('data/raw/crop-prices.tsv')
+        open('data/raw/apri_ap_crpouta.tsv')
 
     def test_invalid_url_download_economic_accounts_for_agri(self):
         with self.assertRaises(SystemExit):
             eurostat.download_crop_prices('https://ec.europa.eu/eurostat/web/main/homsdfsde')
+
+    def test_download_crop_categories_dic(self):
+        eurostat.download_crop_categories_dic()
+        open('data/raw/prod_veg.dic')
 
 if __name__ == '__main__':
     unittest.main()
